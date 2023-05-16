c673fa266350ed51de599997f7a4309d1aa7aa1b
alucard23 <alucard23@pop-os.localdomain>
2023-05-16 23:20:57 +0530

    fixed route locking

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -2,5 +2,12 @@ import { useSearchQuery } from "~/stores/myStore"
 
 
 export default defineNuxtRouteMiddleware((to, from) => {
-    console.log()
+    if(!process.server) {
+        const store = useSearchQuery()
+        console.log('auth', store.loggedIn)
+        if((to.fullPath==='/dashboard' || to.fullPath==='/createItem') && store.loggedIn === '0')
+            return navigateTo('/login')
+        else if((to.fullPath==='/signUp' || to.fullPath==='/login') && store.loggedIn === '1')
+            return navigateTo('/dashboard')
+    }
 })
